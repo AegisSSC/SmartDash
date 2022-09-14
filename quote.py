5187925f02f52715d0cec2a33879dceeae25e58c
Brandon Wingard <wingardbrandonm@gmail.com>
2022-09-14 15:53:32 -0400

    Beginning Stock Aggregation

--- quote.py
+++ quote.py
@@ -1,6 +1,5 @@
 #THIS IS FOR THE QUOTE
 import random
-import time 
 #Connect to DB
 
 list = []
@@ -9,8 +8,8 @@ r = random.sample(range(0,(365*4)), k=365)
 
 
 # Get the quote that belongs at the given location
-def get_quote(i):
-    day = r[i]
+def get_quote(day):
+    day = r[day]
     query = 'SELECT quote FROM quotetable WHERE day = %t'
 
     # execute query
--- stocks/stocks.py
+++ stocks/stocks.py
@@ -5,10 +5,29 @@
 
 
 
-import quandl as q
+import quandl
 import numpy as np
 import pandas as pd
 
+
+
+all_stocks = {'AAPL', 'GOOGL', 'TSLA', 'F', 'NVDA', 'VZ'}
+start_dates = { 'AAPL' : "2010-01-01", 
+                'GOOGL' : "2010-01-01", 
+                'TSLA' : "2010-01-01", 
+                'F' : "2010-01-01", 
+                'NVDA' : "2010-01-01", 
+                'VZ' : "2010-01-01"}
+end_dates = { 'AAPL' : "2013-01-01", 
+              'GOOGL' : "2013-01-01", 
+              'TSLA' : "2013-01-01",
+              'F' : "2013-01-01", 
+              'NVDA' : "2013-01-01",
+              'VZ' : "2013-01-01"}
+
+def scrape_data(company, start, end):
+    stock_data = quandl.get("WIKI/"+ company, start_date=start, end_date=end)
+    stock_data.to_csv('stock_info/'+company+'.csv')
 
 
 
